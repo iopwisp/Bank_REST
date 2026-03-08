2b7a793d6e7d9b730502dbfc9d37fdd213a836f8
iopwisp <@.com>
2026-03-09 01:14:18 +0500

    add frontend

--- src/main/java/com/example/bankcards/repository/CardRepository.java
+++ src/main/java/com/example/bankcards/repository/CardRepository.java
@@ -5,12 +5,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface CardRepository extends JpaRepository<Card , Long> {
-
-    boolean existsByCardNumber(String cardNumber);
 
     Page<Card> findByUserId (Long user_id, Pageable pageable);
 
